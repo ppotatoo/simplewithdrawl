8512f266ffb151b35f57881960e95bce066d73f1
ppotatoo <rkawas731@protonmail.com>
2021-01-03 19:15:52 -0500

    if you dont enter a correct name its weird

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # simplewithdrawl
+
+why are you here this is bad code.
+
+but if you have changes make em
+
+since I know its bad lmao
--- simplewithdrawl.py
+++ simplewithdrawl.py
@@ -4,14 +4,14 @@ with open("data.json", "r") as f:
         data = json.load(f)
 
 def start():
-    choice = input('Select your choice. Type \'1\' for Reading, and \'2\' for Writing. \n')
+    choice = input('Select your choice. Type \'1\' for Reading, and \'2\' for Editing. \n')
     if choice == '1':
-        name = input('Who\'s name would you like to read information from? \n')
-        if name not in data:
+        name = input('Select a person to read information from. \n')
+        if name not in data['Data']:
             print('It looks like you did not enter a valid name. Please try again.')
             startover()
         else:
-            info = input('What information would you like to read from this person? You may choose \'Age\', \'Gender\', or \'Profession\'. \n')
+            info = input(f'What information would you like to read from {name}? You may choose \'Age\', \'Gender\', or \'Profession\'. You may also type \'All\' to view all information \n')
             info = info.capitalize()
             if info == 'Age':
                 print(f'{name}\'s age is set to '+data['Data'][name][info])
@@ -19,10 +19,33 @@ def start():
                 print(f'{name}\'s gender is set to '+data['Data'][name][info])
             if info == 'Profession':
                 print(f'{name}\'s profession is set to '+data['Data'][name][info])
+            if info == 'All':
+                print(data['Data'][name])
             else:
-                print('It looks like you did not select one of the availible categories.' )
+                print('It looks like you did not select one of the available categories.' )
                 startover()
-
+    if choice == '2':
+        name = input('Select a person to edit: \n')
+        if name not in data['Data']:
+            print('It looks like you did not enter a valid name. Please try again.')
+            startover()
+        else:
+            with open("data.json", "w") as f:
+                info = input(f'What information would you like to edit from {name}? You may choose \'Age\', \'Gender\', or \'Profession\'. \n')
+                info = info.capitalize()
+                if info == 'Age':
+                    data['Data'][name][info] = input(f'What would you like to set {name}\'s age to? \n')
+                    json.dump(data, f, indent=4)
+                    print(f'Set {name}\'s age to '+data['Data'][name][info])
+                if info == 'Gender':
+                    data['Data'][name][info] = input(f'What would you like to set {name}\'s gender to? \n')
+                    json.dump(data, f, indent=4)
+                    print(f'Set {name}\'s gender to '+data['Data'][name][info])
+                if info == 'Profession':
+                    data['Data'][name][info] = input(f'What would you like to set {name}\'s profession to? \n')
+                    json.dump(data, f, indent=4)
+                    print(f'Set {name}\'s profession to '+data['Data'][name][info])
+    startover()
 
 def startover():
     startchoice = input('Would you like to restart? Type \'Yes\' or \'No\' \n')
